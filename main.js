19c8944dc2f4b90c0d913f9a4bd58ced84c59ce5
ahmed <ahmedmijdo2@gmail.com>
2024-10-05 09:07:28 +0300

    V1.1 adding Choose the Current

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-let usaCurrent = document.getElementById("first");
+let Current = document.getElementById("first");
 let result = document.getElementById("second");
+let select = document.getElementById("select");
 let transform = document.getElementById("btn");
 
 transform.addEventListener("click" , function() {
-    let usaCurrentValue = usaCurrent.value;
-    if (usaCurrentValue === " " || isNaN(parseInt(usaCurrentValue))) {
-        console.log(false);
-    } else if (!isNaN(parseInt(usaCurrentValue))) {
-        window.sessionStorage.setItem("usaCurrent" , usaCurrentValue);
-    }
-    let getValue = window.sessionStorage.getItem("usaCurrent");
-    result.value =  getValue * 50;
-});
+    let CurrentValue = Current.value;
+    let selectValue = select.value;
 
+    if (CurrentValue === " " || isNaN(parseInt(CurrentValue))) {
+        console.log(false);
+    } else if (!isNaN(parseInt(CurrentValue))) {
+        window.sessionStorage.setItem("Current" , CurrentValue);
+    }
+    let getValue = window.sessionStorage.getItem("Current");
+
+    
+    // select the current
+    if (selectValue === "USA") {
+        result.value =  getValue * 50;
+    } else if (selectValue === "EURO") {
+        result.value =  getValue * 52;
+    } else if (selectValue === "CNY") {
+        result.value =  getValue * 6;
+    }
+});
 
 
 
